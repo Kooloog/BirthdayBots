1092d78b6c2c00a9e3c3a35d0d3942208d2e8558
RobertoMMA <roberto.morenomar@alum.uca.es>
2021-07-14 18:47:50 +0200

    Commented main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+# This is the program that the server executing this software is constantly running.
+
 import os
 import time
 import schedule
 
 
+# Very simple function that simply runs the important code
 def run_code():
     os.system("python3 post_to_bot.py")
 
 
+# This line executes the above function every day at 2pm CEST (my timezone)
 schedule.every().day.at("14:00").do(run_code)
 
+# And now we wait :)
 while True:
     schedule.run_pending()
     time.sleep(1)
